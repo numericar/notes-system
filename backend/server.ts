3dc8b462e5e0da4f62a38ade5ae38d1df08697ac
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-07 18:52:03 +0700

    setup envalid

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,8 @@
+import "dotenv/config";
+import env from "./utils/validateEnv";
+
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -6,7 +10,14 @@ app.get("", (req: express.Request, res: express.Response): void => {
     res.send("Hello World");
 });
 
-const port: number = 5000;
-app.listen(port, (): void => {
-    console.log(`Server listening at http://localhost:${port}`);
+mongoose.connect(env.MONGO_CONNECTION_STRING).then((): void => {
+    console.log("Database Connected");
+}).catch((error) => {
+    console.log(error);
 });
+
+app.listen(env.PORT, (): void => {
+    console.log(`Server listening at http://localhost:${env.PORT}`);
+});
+
+// https://youtu.be/FcxjCPeicvU?si=FSMec66tPg0TKVfe&t=4451
