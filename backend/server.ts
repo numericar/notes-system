e9cc5a4f92602f57894a6c8ffbf60d4f27ac18d4
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-01-07 15:29:48 +0700

    setup dependency

--- backend/server.ts
+++ backend/server.ts
@@ -3,10 +3,10 @@ import express from "express";
 const app = express();
 
 app.get("", (req: express.Request, res: express.Response): void => {
-    res.send("Hello World")
+    res.send("Hello World");
 });
 
-const port = 5000;
+const port: number = 5000;
 app.listen(port, (): void => {
     console.log(`Server listening at http://localhost:${port}`);
 });
